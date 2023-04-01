bdd08740b377fff72fbdff016e259e5a76100f39
Vitalii Kravchenko <vitalyx@gmail.com>
2023-04-01 16:59:32 +0100

    Add type generation and context value

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,11 @@ import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import { hash } from './hash';
 import { readFileSync } from 'fs';
+import { Resolvers } from './generated/graphql';
 
 /**
- * The following lines intialize dotenv,
- * so that env vars from the .env file are present in process.env
+ * The following lines intialize `dotenv`,
+ * so that env vars from the .env file are present in `process.env`.
  */
 dotenv.config();
 
@@ -69,7 +70,7 @@ const API = 'https://dog.ceo/api';
 // Provide resolver functions for your schema fields.
 // Resolvers tell Apollo Server how to fetch the data associated with a particular type.
 // Here we just wrap the `dog.ceo` REST-based API.
-const resolvers = {
+const resolvers: Resolvers = {
   Query: {
     dogs: async () => {
       const results = await fetch(`${API}/breeds/list/all`);
@@ -108,9 +109,31 @@ const resolvers = {
 
 const typeDefs = readFileSync('./schema.graphql', { encoding: 'utf-8' });
 
+// To setup code generation:
+//   yarn add -D @graphql-codegen/cli
+//               @graphql-codegen/typescript
+//               @graphql-codegen/typescript-resolvers
+//               @graphql-codegen/introspection
+//   npx graphql-code-generator init
+
+// During a GraphQL operation, you can share data throughout your server's resolvers and plugins
+// by creating an object named `contextValue`. You can pass useful things through your `contextValue`
+// that any resolver might need, like authentication scope, sources for fetching data, database connections,
+// and custom fetch functions. If you're using dataloaders to batch requests across resolvers,
+// you can also attach them to the shared `contextValue`.
+// https://www.apollographql.com/docs/apollo-server/data/context/
+
+// Your server calls the `context` function once for every request,
+// enabling you to customize your `contextValue` with each request's details (such as HTTP headers).
+
+// Types for our `contextValue`:
+interface SandboxContext {
+  authScope?: string;
+}
+
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
-const server = new ApolloServer({
+const server = new ApolloServer<SandboxContext>({
   typeDefs,
   resolvers,
   introspection: true, // it's recommended to disable this in production environment
@@ -122,6 +145,9 @@ const server = new ApolloServer({
 //  3. prepares your app to handle incoming requests
 const { url } = await startStandaloneServer(server, {
   listen: { port: 4000 },
+  context: async ({ req, res }) => ({
+    authScope: req.headers.authorization,
+  }),
 });
 
 console.log(`🚀  Server ready at: ${url}`);
---
+++ src/generated/graphql.ts
@@ -0,0 +1,173 @@
+import { GraphQLResolveInfo } from 'graphql';
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type RequireFields<T, K extends keyof T> = Omit<T, K> & { [P in K]-?: NonNullable<T[P]> };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+};
+
+export enum CacheControlScope {
+  Private = 'PRIVATE',
+  Public = 'PUBLIC'
+}
+
+export type Dog = {
+  __typename?: 'Dog';
+  breed: Scalars['String'];
+  displayImage?: Maybe<Scalars['String']>;
+  id: Scalars['String'];
+  images?: Maybe<Array<Maybe<Image>>>;
+  subbreeds?: Maybe<Array<Maybe<Scalars['String']>>>;
+};
+
+export type Image = {
+  __typename?: 'Image';
+  id: Scalars['String'];
+  url: Scalars['String'];
+};
+
+export type Query = {
+  __typename?: 'Query';
+  dog?: Maybe<Dog>;
+  dogs?: Maybe<Array<Maybe<Dog>>>;
+};
+
+
+export type QueryDogArgs = {
+  breed: Scalars['String'];
+};
+
+
+
+export type ResolverTypeWrapper<T> = Promise<T> | T;
+
+
+export type ResolverWithResolve<TResult, TParent, TContext, TArgs> = {
+  resolve: ResolverFn<TResult, TParent, TContext, TArgs>;
+};
+export type Resolver<TResult, TParent = {}, TContext = {}, TArgs = {}> = ResolverFn<TResult, TParent, TContext, TArgs> | ResolverWithResolve<TResult, TParent, TContext, TArgs>;
+
+export type ResolverFn<TResult, TParent, TContext, TArgs> = (
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => Promise<TResult> | TResult;
+
+export type SubscriptionSubscribeFn<TResult, TParent, TContext, TArgs> = (
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => AsyncIterable<TResult> | Promise<AsyncIterable<TResult>>;
+
+export type SubscriptionResolveFn<TResult, TParent, TContext, TArgs> = (
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => TResult | Promise<TResult>;
+
+export interface SubscriptionSubscriberObject<TResult, TKey extends string, TParent, TContext, TArgs> {
+  subscribe: SubscriptionSubscribeFn<{ [key in TKey]: TResult }, TParent, TContext, TArgs>;
+  resolve?: SubscriptionResolveFn<TResult, { [key in TKey]: TResult }, TContext, TArgs>;
+}
+
+export interface SubscriptionResolverObject<TResult, TParent, TContext, TArgs> {
+  subscribe: SubscriptionSubscribeFn<any, TParent, TContext, TArgs>;
+  resolve: SubscriptionResolveFn<TResult, any, TContext, TArgs>;
+}
+
+export type SubscriptionObject<TResult, TKey extends string, TParent, TContext, TArgs> =
+  | SubscriptionSubscriberObject<TResult, TKey, TParent, TContext, TArgs>
+  | SubscriptionResolverObject<TResult, TParent, TContext, TArgs>;
+
+export type SubscriptionResolver<TResult, TKey extends string, TParent = {}, TContext = {}, TArgs = {}> =
+  | ((...args: any[]) => SubscriptionObject<TResult, TKey, TParent, TContext, TArgs>)
+  | SubscriptionObject<TResult, TKey, TParent, TContext, TArgs>;
+
+export type TypeResolveFn<TTypes, TParent = {}, TContext = {}> = (
+  parent: TParent,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => Maybe<TTypes> | Promise<Maybe<TTypes>>;
+
+export type IsTypeOfResolverFn<T = {}, TContext = {}> = (obj: T, context: TContext, info: GraphQLResolveInfo) => boolean | Promise<boolean>;
+
+export type NextResolverFn<T> = () => Promise<T>;
+
+export type DirectiveResolverFn<TResult = {}, TParent = {}, TContext = {}, TArgs = {}> = (
+  next: NextResolverFn<TResult>,
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => TResult | Promise<TResult>;
+
+
+/** Mapping between all available schema types and the resolvers types */
+export type ResolversTypes = {
+  Boolean: ResolverTypeWrapper<Scalars['Boolean']>;
+  CacheControlScope: CacheControlScope;
+  Dog: ResolverTypeWrapper<Dog>;
+  Image: ResolverTypeWrapper<Image>;
+  Int: ResolverTypeWrapper<Scalars['Int']>;
+  Query: ResolverTypeWrapper<{}>;
+  String: ResolverTypeWrapper<Scalars['String']>;
+};
+
+/** Mapping between all available schema types and the resolvers parents */
+export type ResolversParentTypes = {
+  Boolean: Scalars['Boolean'];
+  Dog: Dog;
+  Image: Image;
+  Int: Scalars['Int'];
+  Query: {};
+  String: Scalars['String'];
+};
+
+export type CacheControlDirectiveArgs = {
+  inheritMaxAge?: Maybe<Scalars['Boolean']>;
+  maxAge?: Maybe<Scalars['Int']>;
+  scope?: Maybe<CacheControlScope>;
+};
+
+export type CacheControlDirectiveResolver<Result, Parent, ContextType = any, Args = CacheControlDirectiveArgs> = DirectiveResolverFn<Result, Parent, ContextType, Args>;
+
+export type DogResolvers<ContextType = any, ParentType extends ResolversParentTypes['Dog'] = ResolversParentTypes['Dog']> = {
+  breed?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  displayImage?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
+  id?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  images?: Resolver<Maybe<Array<Maybe<ResolversTypes['Image']>>>, ParentType, ContextType>;
+  subbreeds?: Resolver<Maybe<Array<Maybe<ResolversTypes['String']>>>, ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+};
+
+export type ImageResolvers<ContextType = any, ParentType extends ResolversParentTypes['Image'] = ResolversParentTypes['Image']> = {
+  id?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  url?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+};
+
+export type QueryResolvers<ContextType = any, ParentType extends ResolversParentTypes['Query'] = ResolversParentTypes['Query']> = {
+  dog?: Resolver<Maybe<ResolversTypes['Dog']>, ParentType, ContextType, RequireFields<QueryDogArgs, 'breed'>>;
+  dogs?: Resolver<Maybe<Array<Maybe<ResolversTypes['Dog']>>>, ParentType, ContextType>;
+};
+
+export type Resolvers<ContextType = any> = {
+  Dog?: DogResolvers<ContextType>;
+  Image?: ImageResolvers<ContextType>;
+  Query?: QueryResolvers<ContextType>;
+};
+
+export type DirectiveResolvers<ContextType = any> = {
+  cacheControl?: CacheControlDirectiveResolver<any, any, ContextType>;
+};
